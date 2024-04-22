f17de6a7de0e6fdf640b03335c20b7c2abaf7f67
Rishi Kumar <rsi.dev17@gmail.com>
2024-04-23 00:36:44 +0530

    feat: add requirements.txt

--- run.sh
+++ run.sh
@@ -1,3 +1,12 @@
 # Update this file to run your own code
 
+# # create python virtual environment
+# python3 -m venv venv
+
+# # activate the virtual environment
+# source venv/bin/activate
+
+# install the required packages
+pip install -r requirements.txt
+
 python -m src.main
